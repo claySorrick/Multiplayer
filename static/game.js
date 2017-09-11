567f5d730624621a6d39bf242eb5c2a3db7da47a
claySorrick <billiard117@yahoo.com>
2017-09-11 16:26:26 -0700

    test using buttons

--- static/game.js
+++ static/game.js
@@ -5,7 +5,15 @@ function clickHostRole() {
 }
 
 function clickPlayerRole() {
-	document.getElementById("canvasDiv").innerHTML = "";
+	document.getElementById("canvasDiv").innerHTML = "<div style=\"text-align:center;\"><button onmousedown=\"moveUp()\">UP</button></div>" +
+	"<div style=\"text-align:center;\"><button onmousedown=\"moveLeft()\">LEFT</button>" +
+	"<button onmousedown=\"moveRight()\">RIGHT</button></div>" +
+	"<div style=\"text-align:center;\"><button onmousedown=\"moveDown()\">DOWN</button></div>";
+	
+	// <button ontouchstart="moveup()" onmousedown="moveup()" onmouseup="clearmove()">UP</button><br><br>
+  // <button ontouchstart="moveleft()" onmousedown="moveleft()" onmouseup="clearmove()">LEFT</button>
+  // <button ontouchstart="moveright()" onmousedown="moveright()" onmouseup="clearmove()">RIGHT</button><br><br>
+  // <button ontouchstart="movedown()" onmousedown="movedown()" onmouseup="clearmove()">DOWN</button></div>
     document.getElementById("roleButtons").innerHTML = "";
 }
 
@@ -16,36 +24,35 @@ var movement = {
   right: false
 }
 
-// function clickDown(position){
-	// if (event.clientx<document.body.clientWidth/3){
+function moveUp(){
+	movement.up = true;
+}
+function moveDown(){
+	movement.down = true;
+}
+function moveLeft(){
+	movement.left = true;
+}
+function moveRight(){
+	movement.right = true;
+}
+
+
+// document.addEventListener('mousedown', function(event) {
+	// console.log(event);
+  // if (event.clientX<document.body.clientWidth/3){
 		// movement.left = true;
 	// }
-	// else if (event.clientx> 2 * (document.body.clientWidth/3)){
+	// else if (event.clientX> 2 * (document.body.clientWidth/3)){
 		// movement.right = true;
 	// }
-	// else if (event.clienty> document.body.clientWidth/2){
+	// else if (event.clientY> document.body.clientHeight/2){
 		// movement.down = true;
 	// }
 	// else{
 		// movement.up = true;
 	// }
-// }
-
-document.addEventListener('mousedown', function(event) {
-	console.log(event);
-  if (event.clientX<document.body.clientWidth/3){
-		movement.left = true;
-	}
-	else if (event.clientX> 2 * (document.body.clientWidth/3)){
-		movement.right = true;
-	}
-	else if (event.clientY> document.body.clientHeight/2){
-		movement.down = true;
-	}
-	else{
-		movement.up = true;
-	}
-});
+// });
 document.addEventListener('mouseup', function(event) {
       movement.left = false;
       movement.up = false;
